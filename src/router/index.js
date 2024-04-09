576e5311e1756f1fb89d4333704abe815f4bcb6f
GyozaOtoko <133895259+GyozaOtoko@users.noreply.github.com>
2024-04-09 00:15:07 -0400

    Layout update

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/HomePage.vue';
-import Collection from '../views/CollectionPage.vue';
-import Natmonkeys from '../views/NatmonkeysPage.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import OMBiz from "../views/OMBizPage.vue";
+import Collection from "../views/CollectionPage.vue";
+import Natmonkeys from "../views/NatmonkeysPage.vue";
+import HomePage from "../views/HomePage.vue";
 
 const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: Home,
-    },
-    {
-        path: '/collection',
-        name: 'collection',
-        component: Collection,
-    },
-    {
-        path: '/natmonkeys',
-        name: 'natmonkeys',
-        component: Natmonkeys,
-    },
+  {
+    path: "/",
+    name: "Home",
+    component: HomePage,
+  },
+  {
+    path: "/ombiz",
+    name: "OMBiz",
+    component: OMBiz,
+  },
+  {
+    path: "/collection",
+    name: "collection",
+    component: Collection,
+  },
+  {
+    path: "/natmonkeys",
+    name: "natmonkeys",
+    component: Natmonkeys,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes,
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
 });
 
 export default router;
--- src/views/NatmonkeysPage.vue
+++ src/views/NatmonkeysPage.vue
@@ -5,33 +5,44 @@
                 data-aos-easing="ease-in-out" data-aos-duration="800" data-aos-once="true">
                 <div class="container">
                     <div class="header-content col-md-12">
-                        <div class="row fade-up" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="800" data-aos-once="true">
+                        <div class="row fade-up" data-aos="fade-up" data-aos-easing="ease-in-out"
+                            data-aos-duration="800" data-aos-once="true">
                             <div class="col-md-7 align-self-center">
                                 <h1>Natmonkeys</h1>
                                 <p><b>8064 Monkeys generated straight from the Bitcoin blockchain!</b></p>
                                 <p>Natmonkey #:</p>
-                                <input id="blk" type="number" v-model="blockNumber" @input="onBlockChange"/>
+                                <input id="blk" type="number" v-model="blockNumber" @input="onBlockChange" />
                                 <div id="cigDiv" class="cig m-2" v-show="showCigCheckbox">
-                                    <input id="cig" style="padding:20px" @click="checkCig" type="checkbox" v-model="cigChecked"/>
-                                    <label for="cig" style="color: #fff; padding-left: 20px; font-size: 12px">Show Cigarette</label>
+                                    <input id="cig" style="padding:20px" @click="checkCig" type="checkbox"
+                                        v-model="cigChecked" />
+                                    <label for="cig" style="color: #fff; padding-left: 20px; font-size: 12px">Show
+                                        Cigarette</label>
                                 </div>
                                 <div class="mt-4">
-                                    <a href="https://magiceden.io/ordinals/marketplace/natmonkeys" target="_blank" class="btn btn-warning" role="button">Adopt a Natmonkey</a>
+                                    <a href="https://magiceden.io/ordinals/marketplace/natmonkeys" target="_blank"
+                                        class="btn btn-warning" role="button">Adopt a Natmonkey</a>
                                 </div>
                             </div>
-                            <div id="viewerWrapper" class="d-flex col-md-5" style="min-height: 400px;min-width: 400px; margin-top: 50px; cursor: pointer" @click="download">
-                                <div id="natmonkeysviewer" style="position: relative;width: 100%;height: 100%;" v-html="htmlContent"></div>
+                            <div id="viewerWrapper" class="d-flex col-md-5"
+                                style="min-height: 400px;min-width: 400px; margin-top: 50px; cursor: pointer"
+                                @click="download">
+                                <div id="natmonkeysviewer" style="position: relative;width: 100%;height: 100%;"
+                                    v-html="htmlContent"></div>
                             </div>
                         </div>
                     </div>
                     <div class="mt-4">
-                        <a href="https://ordinal-monkey-business.gitbook.io/natmonkeys" class="btn" style="color: #fff" target="_blank"><i class="icofont-diamond" style="vertical-align: text-top"></i> Traits & Rarities</a>
+                        <a href="https://ordinal-monkey-business.gitbook.io/natmonkeys" class="btn" style="color: #fff"
+                            target="_blank"><i class="icofont-diamond" style="vertical-align: text-top"></i> Traits &
+                            Rarities</a>
                     </div>
                 </div>
             </div>
         </div>
         <Transition name="fade">
-            <div id="modalWrapper" v-show="showModal" style="position: absolute;height: 100vh;width: 100vw;background-color:rgba(0,0,0, 0.5);top: 0;display: flex;justify-content: center;align-items: center" @click="showModal = false">
+            <div id="modalWrapper" v-show="showModal"
+                style="position: absolute;height: 100vh;width: 100vw;background-color:rgba(0,0,0, 0.5);top: 0;display: flex;justify-content: center;align-items: center"
+                @click="showModal = false">
                 <div id="modal">
                     <canvas id="myCanvas" style="border:2px solid black;" :width="width" :height="height"></canvas>
                 </div>
@@ -65,13 +76,13 @@ export default {
     components: {
     },
     computed: {
-        originalString: function() {
-            if (this.blockNumber) 
+        originalString: function () {
+            if (this.blockNumber)
                 return this.blockNumber.toString()
             else return ''
         },
-        showCigCheckbox: function() {
-            if (this.originalString.length >=4) {
+        showCigCheckbox: function () {
+            if (this.originalString.length >= 4) {
                 if (this.originalString.padStart(7, '0').split('').map(Number).reverse()[3] == 4)
                     return true
             }
@@ -87,28 +98,28 @@ export default {
         },
         download() {
             console.log('download clicked');
-            this.renderHtmlToCanvas( document.getElementById( 'myCanvas' ), generateHtmlBasedOnBlockNumber(this.blockNumber, `1024px`, `1024px`, this.cigChecked) );
+            this.renderHtmlToCanvas(document.getElementById('myCanvas'), generateHtmlBasedOnBlockNumber(this.blockNumber, `1024px`, `1024px`, this.cigChecked));
             this.showModal = true;
         },
         renderHtmlToCanvas(canvas, html) {
-            const ctx = canvas.getContext( '2d' );
+            const ctx = canvas.getContext('2d');
             const svg = `
             <svg xmlns="http://www.w3.org/2000/svg" width="1024" height="1024">
             <foreignObject width="100%" height="100%">
             <div xmlns="http://www.w3.org/1999/xhtml">${html}</div>
             </foreignObject>
             </svg>`;
-            const svgBlob = new Blob( [svg], { type: 'image/svg+xml;charset=utf-8' } );
-            const svgObjectUrl = URL.createObjectURL( svgBlob );
+            const svgBlob = new Blob([svg], { type: 'image/svg+xml;charset=utf-8' });
+            const svgObjectUrl = URL.createObjectURL(svgBlob);
             const tempImg = new Image();
-            tempImg.addEventListener( 'load', function() {
-                ctx.drawImage( tempImg, 0, 0 );
-                URL.revokeObjectURL( svgObjectUrl );
-            } );
+            tempImg.addEventListener('load', function () {
+                ctx.drawImage(tempImg, 0, 0);
+                URL.revokeObjectURL(svgObjectUrl);
+            });
 
             tempImg.src = svgObjectUrl;
         },
-        checkCig(){
+        checkCig() {
             this.cigChecked = !this.cigChecked;
             this.update()
         }
@@ -119,24 +130,26 @@ export default {
 };
 </script>
 <style>
-    .cls-5 {
-        mix-blend-mode:multiply;
-        opacity:.4
-    }
-    .layer {
-        position: absolute;
-        top: 0px;
-        left: 0px;
-        image-rendering: pixelated;
-        height: 100%;
-    }
-    .fade-enter-active,
-    .fade-leave-active {
-    transition: opacity 0.5s ease;
-    }
+.cls-5 {
+    mix-blend-mode: multiply;
+    opacity: .4
+}
 
-    .fade-enter-from,
-    .fade-leave-to {
+.layer {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    image-rendering: pixelated;
+    height: 100%;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
     opacity: 0;
-    }
+}
 </style>
